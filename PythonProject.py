0d6546b78911a975e0b4a967f544c4a395cd261f
Blusmj <smj163@scarletmail.rutgers.edu>
2018-11-15 16:19:30 -0500

    Choice/Story
    Added more to the story and choice

--- PythonProject.py
+++ PythonProject.py
@@ -1,27 +1,60 @@
 import time
 import random
+#import image small drawings are going to be used for if I attatch pictures to the project
 
-def ask_YN(question): #Ask yes or no question
-   answer_YN=("yes","no") #Used for yes or no choices
-   response= none
-   while response not in answer_YN:
-      response = input (question) not in answer_YN
-      if response not in answer_YN:  #if it isn't yes or no prompts below code
-         print("Not a valid response. Please Input yes or no.")
-      return response
 
-def intro(): #introduction
-    print("Lorem ipsum dolor sit amet.")
+def ch1():
+   ch1=("yes","Yes","No","no") #Used for yes or no choices
+
+def intro(): #Used to start the code and define
+    print("...")
     time.sleep(2) #gives intervals of 2 seconds between prints
-    print("consectetur adipiscing elit. ")
+    print("... ")
     time.sleep(2)
-    print (" Cras vel nisl ac leo tristique luctus in eu odio.")
+    print ("You feel as though your floating.\n")
     time.sleep(3)
-    print ("Mauris massa ligula, dapibus at tincidunt lobortis, consequat non est.")
-    intro()
+    print ("You attempt to feel around, but it feels as though your arms don't exist.\n")
+    time.sleep(2)
+    print ("What has happened to you?\n")
+    time.sleep(4)
+    print ("You feel that the body you moved in freely has been snatched away in the span of seconds\n")
+    time.sleep(3)
+    print ("The last thing you remember was the roaring of planes overhead and ear piercing whistle-like sounds.\n")
+    time.sleep(2)
+    print ("The thought of the memory gave a chilling sensation.\n")
+    time.sleep(2)
+    print ("After some time you see a light.\n")
+    time.sleep(3)
+    print ("With all the strength you had to muster you try to move towards it.\n ")
+    time.sleep(2)
+    print ("Each movement towards the light made you feel more strange than the last.\n")
+    time.sleep(2)
+    print ("As if your body was turning into jello.\n")
+    time.sleep(2)
+    print ("You emerge from the light and were greeted by rocks and giant blue crystal resting in the middle of the cave.\n ")
+    time.sleep(3)
+    print ("You take a step towards it...\n")
+    time.sleep(3)
+    print ("But immediately you notice that you don't have legs.\n")
+    time.sleep(3)
+    print ("You try to move as you used to, but to no avail.\n")
+    time.sleep(2)
+    print ("After trial and error you figure out that you can roll, but found that hopping around was more efficient.")
+    time.sleep(3)
+    print ("as you never felt tired or that you were straining yourself jumping up and down.\n")
+    time.sleep(2)
+    print ("After getting used to whatever your squishy feeling body turned into you face back towards the crystals.\n")
+
+    ch1=str(raw_input("Will you take the crystals?(Y/N)\n"))#Choice 1
+    if ch1 == "yes" or "Yes": #Selecting yes shows different events
+      print ("Attempting to touch the crystals, they inturn dissolved inside of you after contact.\n")
+    elif ch1 == "no" or "No":#Selecting no chooses different events  as well
+      print ("You look at the crystal, but a strong feeling in your gut told you not to mess with it.\n")
 
 
-class item:
-   def__init__(self, item_type, name)
-      self.item_type = item_type
-      self.name = name
+#class self:
+ #def__init__(self, item_type, name)
+ #self.self_type = self_type
+ #self.name = name
+ #
+intro()
